b9493c143fa9f0ddf360d5c58effedde02320a9a
cakirseymanur <seyma_akay97@hotmail.com>
2022-12-25 14:09:07 +0300

    Currency

---
+++ Api_Consume/Controllers/CurrencyController.cs
@@ -0,0 +1,36 @@
+﻿using Api_Consume.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Api_Consume.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "8353feee66mshf5988d224e46636p1a9300jsncc4fab7ab9cd" },
+		{ "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				var currencies = JsonConvert.DeserializeObject<CurrencyListViewModel.Rootobject>(body);
+				return View(currencies.exchange_rates);
+			}
+        }
+    }
+}
